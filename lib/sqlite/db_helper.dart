e9fb91632d3f84cd572749d6d64aeb29e2aa71bf
PQ777 <phanqui2021@gmail.com>
2022-01-21 11:17:19 +0700

    - Commit model/cart_model.dart - Commit database helper

---
+++ lib/sqlite/db_helper.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'dart:io' as io;
+import 'package:demo/model/cart_model.dart';
+
+class DBhelper{
+  static Database? _db;
+
+  Future<Database?> get db async{
+    if(_db != null){
+      return _db!;
+    }
+
+    _db = await initDatabase();
+  }
+
+  initDatabase() async {
+    io.Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, "cart.db");
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return db;
+  }
+
+  _onCreate(Database db, int version) async{
+    await db.execute(
+        'CREATE TABLE cart ('
+            '  id INTEGER PRIMARY KEY '
+            ', productId VARCHAR UNIQUE'
+            ', productName TEXT'
+            ', initialPrice INTEGER'
+            ', productPrice INTEGER '
+            ', quantity INTEGER'
+            ', unitTag TEXT '
+            ', image TEXT )');
+  }
+
+  Future<Cart> insert(Cart cart)async{
+    var dbClient = await db;
+    await dbClient!.insert('cart', cart.toMap());
+    return cart;
+  }
+
+}
