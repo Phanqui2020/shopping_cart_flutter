f2799c8e46a8856d383f31507ca9775c3a5462af
phanqui <phanqui20202gmail.com>
2022-01-25 14:07:16 +0700

    - fix model/cart_model.dart - insert and update cart - add provider

---
+++ lib/Provider/CartProvide.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CartProvider with ChangeNotifier{
+  int _counter =0;
+
+  int get counter => _counter;
+
+  double _totalPrice = 0.0;
+
+  double get totalPrice => _totalPrice;
+
+  void _setPreItems()async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('cart_item', _counter);
+    prefs.setDouble('total_price', _totalPrice);
+    notifyListeners();
+  }
+
+  void _getPreItems()async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.getInt('cart_item') ?? 0;
+    prefs.getDouble('total_price') ?? 0.0;
+    notifyListeners();
+  }
+
+  void addCounter(){
+    _counter++;
+    _setPreItems();
+  }
+
+  void removeCounter(){
+    _counter--;
+    _setPreItems();
+  }
+
+  int getCounter(){
+    _getPreItems();
+    return _counter;
+  }
+
+  void addTotalPrice(double productPrice){
+    _totalPrice = _totalPrice + productPrice;
+    _setPreItems();
+  }
+
+  void removeTotalPrice(double productPrice){
+    _totalPrice = _totalPrice - productPrice;
+    _setPreItems();
+  }
+
+  double getTotalPrice(){
+    _getPreItems();
+    return _totalPrice;
+  }
+}
